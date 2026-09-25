c4b64dbb05c9ebdd6b72a4fbf3fb3e4cb114505f
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-25 13:04:54 -0300

    Cria a lista de produtos com metodo de adicionar

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@ namespace CadastroProdutos;
 
 class Program
 {
+    static List<Produto> produtos = new List<Produto>();
+
     static void Main(string[] args)
     {
         Console.WriteLine("Cadastro de produtos");
 
-        var produto = new Produto("Caneta", 2.50m, 100);
-        Console.WriteLine(produto);
+        AdicionarProduto("Caneta", 2.50m, 100);
+        AdicionarProduto("Caderno", 15.90m, 30);
+
+        foreach (var p in produtos)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+    static void AdicionarProduto(string nome, decimal preco, int quantidade)
+    {
+        produtos.Add(new Produto(nome, preco, quantidade));
     }
 }
